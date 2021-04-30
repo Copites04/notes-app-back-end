f935fac2c8ce676a68bc2c4af7f2082e4cc589c0
Copites04 <tokokita0412@gmail.com>
2021-04-30 09:07:32 +0700

    Update package to development

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.42.193',
     // Cross-origin resource sharing (CORS).
     routes: {
       cors: {
